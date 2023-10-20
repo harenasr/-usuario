3bc037bd97df2647c0e3722aad0182ce80f23ebf
Henry <henry.arenas.ramirez.contractor@bbva.com>
2023-10-20 15:47:44 -0500

    finish

---
+++ src/main/java/com/car/UsuarioController.java
@@ -0,0 +1,51 @@
+package com.car;
+
+
+import com.car.entity.Usuario;
+import com.car.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/usuarios") // Ruta base para este controlador
+public class UsuarioController {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public UsuarioController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+        if (usuario.isPresent()) {
+            return ResponseEntity.ok(usuario.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /*
+    @GetMapping("/{nombre}/{password}")
+    public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable String nombre, @PathVariable String contraseña) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+        if (usuario.isPresent()) {
+            return ResponseEntity.ok(usuario.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+     */
+}
